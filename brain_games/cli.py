8fd26cd56596fb94dd4f5b049b999c717d8590a6
agmrv <agmrv@ya.ru>
2020-07-15 16:46:11 +0500

    4th step

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,9 @@
+"""CLI functions."""
+
 import prompt
 
+
 def welcome_user():
+    """Ask the name and print greeting."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+
+"""Script of 'Brain Games'."""
+
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Print greeting."""
     print('Welcome to the Brain Games!\n')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
